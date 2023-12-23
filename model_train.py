b0d2a1a874bdb566b66334d3e84078ee427cc2ad
Simon Abello <abello.simon@gmail.com>
2023-12-23 12:28:32 -0500

    Create the model to train. Will train on Coda

--- model_train.py
+++ model_train.py
@@ -1,2 +1,7 @@
 from ultralytics import YOLO
 
+#Load model 
+license_model = YOLO("yolov8n.yaml") # build a new model 
+
+#Use the model 
+results = license_model.train(data="data.yaml", epochs=11) # train the model
